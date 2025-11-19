d562686b72afe20c56679eb2c815a2bd6351a783
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-11-19 15:15:51 +0000

    feat: re-initialize project with correct backend and amplify config

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "naijalottooracle1c751f03": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
